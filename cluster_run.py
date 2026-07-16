ae201a6d0af4a5c8ba50d1028384707e655ec094
Carolina Ferrer <carolinaf0514@gmail.com>
2026-07-16 17:04:03 -0600

    Remove STEMinists directory from script path

--- cluster_run.py
+++ cluster_run.py
@@ -13,7 +13,7 @@ for state in ['tn', 'co', 'ut', 'pa', 'ma']:
     for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
       for county_alpha in [0, 0.2, 0.4, 0.6, 0.8, 1.0]: # county
         if not cut_alpha == 1.0 and county_alpha == 1.0:
-          jobs.append(f"python ./STEMinists/PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
+          jobs.append(f"python PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
 
 pool = mp.Pool(len(jobs))
 pool.map(run_job, jobs)
