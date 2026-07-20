5676e48a48847ab94a28c70c37e91a8ae811a54d
Carolina Ferrer <carolinaf0514@gmail.com>
2026-07-20 13:30:36 -0600

    just tn
    Comment out the loop for state and its nested loops for parameters.

--- cluster_run.py
+++ cluster_run.py
@@ -14,11 +14,11 @@ for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
         jobs.append(f"python ./PA_ensemble_write_to_file.py 'tn' {comp_alpha} {cut_alpha} {county_alpha}")
 
 
-for state in ['co', 'ut', 'pa', 'ma']:
-   for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
-     for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
-       for county_alpha in [0, 0.2, 0.4, 0.6, 0.8]: # county
-           jobs.append(f"python ./PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
+# for state in ['co', 'ut', 'pa', 'ma']:
+#    for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
+#      for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
+#        for county_alpha in [0, 0.2, 0.4, 0.6, 0.8]: # county
+#            jobs.append(f"python ./PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
 
 pool = mp.Pool(len(jobs))
 pool.map(run_job, jobs)
