ea0138fdd272b92ad6428eae9244f6970bdcb625
Carolina Ferrer <carolinaf0514@gmail.com>
2026-07-23 10:40:13 -0600

    Change state from 'co' to 'ut' in cluster_run.py

--- cluster_run.py
+++ cluster_run.py
@@ -14,7 +14,7 @@ jobs = []
 #         jobs.append(f"python ./PA_ensemble_write_to_file.py 'tn' {comp_alpha} {cut_alpha} {county_alpha}")
 
 
-for state in ['co']:
+for state in ['ut']:
   for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
     for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
       for county_alpha in [0, 0.2, 0.4, 0.6, 0.8]: # county
