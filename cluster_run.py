c81abb8396198e25b08da358471d102edddd040b
Carolina Ferrer <carolinaf0514@gmail.com>
2026-07-29 13:44:20 -0600

    Uncomment job generation logic for states and parameters

--- cluster_run.py
+++ cluster_run.py
@@ -14,12 +14,11 @@ jobs = []
 #         jobs.append(f"python ./PA_ensemble_write_to_file.py 'tn' {comp_alpha} {cut_alpha} {county_alpha}")
 
 
-# for state in ["co", "tn", "pa", "ut", "ma"]:
-#    for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
-#      for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
-#        for county_alpha in [0, 0.2, 0.4, 0.6, 0.8, 1.0]: # county
-#           if ((comp_alpha == 1.0) or (cut_alpha == 1.0) or (county_alpha == 1.0)):
-#             jobs.append(f"python ./PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
-jobs.append("python make_df.py")
+for state in ["co", "tn", "pa", "ut", "ma"]:
+  for comp_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #competitiveness
+    for cut_alpha in [0.5, 0.6, 0.7, 0.8, 0.9, 1.0]: #cut edges
+      for county_alpha in [0, 0.2, 0.4, 0.6, 0.8, 1.0]: # county
+          if ((comp_alpha == 1.0) or (cut_alpha == 1.0) or (county_alpha == 1.0)):
+            jobs.append(f"python ./PA_ensemble_write_to_file.py {state} {comp_alpha} {cut_alpha} {county_alpha}")
 pool = mp.Pool(len(jobs))
 pool.map(run_job, jobs)
